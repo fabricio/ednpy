b3731df67377c2a00286b6cb35cc60ebc1874e27
Fabrício Soares <eu@fabriciosoares.com>
2025-05-21 08:36:12 -0300

    consumo combustivel

---
+++ atividade2/4consumo_combustivel.py
@@ -0,0 +1,10 @@
+# Calculadora de Consumo de Combustível
+# Dados da viagem
+distancia_percorrida = 300 # em quilômetros (km)
+combustivel_gasto = 25 # em litros (1)
+# Cálculo do consumo médio
+consumo_medio = distancia_percorrida / combustivel _gasto
+# Exibição do resultado print ("Dados da viagem:")
+print(f"Distância percorrida: {distancia_percorrida} quilômetros (km)")
+print(f"Combustível gasto: {combustivel_gasto} litros (1)")
+print ("Consumo médio: ", round (consumo_medio, 2), "km/1")
