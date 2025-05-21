7118f0fdd199701a57b807ac78469013f9074f24
Fabrício Soares <eu@fabriciosoares.com>
2025-05-21 08:35:25 -0300

    calcula desconto

---
+++ atividade2/2calcula_desconto.py
@@ -0,0 +1,13 @@
+# Calculadora de desconto
+# Informações do produto
+nome_produto = "Camiseta"
+preco_original = 50.00
+porcentagem_desconto = 20
+# Cálculo do desconto
+valor_desconto = preco_original * (porcentagem _desconto / 100)
+preco_final = preco_original - valor_desconto
+# Exibição dos resultados
+print ("Produto:", nome_produto)
+print(f"Preço Original: R$ {preco_original: .2f}") print ("Desconto:", porcentagem_desconto, "%")
+print(f"Valor de desconto: R$ {valor_desconto: .2f}")
+print(f"Preço final: R$ {preco_final: 2f}")
