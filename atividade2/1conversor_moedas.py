6832774d6471cbab96b23c8ee8eaaa79b930600d
Fabrício Soares <eu@fabriciosoares.com>
2025-05-21 08:35:16 -0300

    conversor moedas

---
+++ atividade2/1conversor_moedas.py
@@ -0,0 +1,12 @@
+#Conversor de moedas.
+# Valores das moedas
+valor_em_reais = 100.00
+taxa_dolar = 5.70
+taxa_euro = 6.40
+# Conversões
+valor_em _dolares = valor_em_reais / taxa_dolar
+valor_em_euros = valor_em_reais / taxa_euro
+# Exibição dos resultados
+print(f"Valor em Reais: R$ {valor_em_reais: .2f}")
+print("Valor em Dólares: $", round(valor_em_dolares, 2))
+print("Valor em Euros: €", round(valor_em_euros, 2))
