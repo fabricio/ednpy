5c1a361ad024dc2306dc0f284ddd79837c48ddc4
Fabrício Soares <eu@fabriciosoares.com>
2025-05-21 08:31:26 -0300

    produto

---
+++ atividade1/4produto.py
@@ -0,0 +1,11 @@
+#Código 4
+nome_produto ='Cadeira de Balanço'
+preco_unitario = 12.40
+quantidade = 3
+
+preco_total = quantidade * preco_unitario
+
+print(f'Produto: {nome_produto}')
+print(f'Preço unitário: R$ {preco_unitario:.2f}')
+print(f'Quantidade: {quantidade}')
+print(f'Preço total: R$ {preco_total:.2f}')
