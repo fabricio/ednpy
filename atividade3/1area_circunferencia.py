12abae2de3796a5f8b4af08608c0e1eaf5273671
Fabrício Soares <eu@fabriciosoares.com>
2025-05-23 19:29:25 -0300

    area circunferência

---
+++ atividade3/1area_circunferencia.py
@@ -0,0 +1,11 @@
+# Constante pi
+pi = 3.14159265
+
+# Entrada do usuário (com ponto flutuante)
+raio = float(input())
+
+# Cálculo da área
+area = pi * (raio ** 2)
+
+# Saída formatada com 4 casas decimais
+print(f"A={area:.4f}")
